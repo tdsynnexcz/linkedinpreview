0a5cf1b6c1464849503afb2903c99059f48932ec
tdsynnexcz <martinhala@outlook.cz>
2025-04-09 13:34:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 
 function generatePreview() {
-  const name = document.getElementById('name').value;
-  const title = document.getElementById('title').value;
-  const content = document.getElementById('content').value.replace(/\n/g, '<br>');
-
-  document.getElementById('preview-name').innerText = name;
-  document.getElementById('preview-title').innerText = title;
-  document.getElementById('preview-content').innerHTML = content;
-  document.getElementById('preview').style.display = 'block';
+  document.getElementById('out-name').innerText = document.getElementById('name').value;
+  document.getElementById('out-title').innerText = document.getElementById('title').value;
+  document.getElementById('out-content').innerText = document.getElementById('content').value;
+  document.getElementById('post').style.display = 'block';
 }
 
 function toggleTheme() {
   document.body.classList.toggle('dark');
 }
 
-function toggleView() {
+function toggleMobile() {
   document.body.classList.toggle('mobile');
 }
